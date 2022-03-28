b5651354846a36252beb9b99ab5d0cdb30863db4
Kerem <Kerem@DESKTOP-6MSCSJG>
2022-03-28 17:04:14 +0300

    Add project files.

---
+++ Methodlar/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Methodlar
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+
+
+            }
+
+            Console.WriteLine("-------Metotlar-------");
+
+            //instance - ornek
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+
+
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+
+
+
+
+
+
+
+        
+
+
+
+    
+
+
---
+++ Methodlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun) 
+        {
+            Console.WriteLine("Sepete eklendi : " + urun.Adi);
+
+
+        
+        
+        }
+
+        
+
+    }
+}
